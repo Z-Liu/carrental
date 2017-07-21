8405a6a93d3be901cacc468c0cdd043c1c5be20f
Z-liu <liuzhao@yeah.net>
2017-07-21 13:48:25 +0800

    20170721

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from frontend.models import Car, Booking
+
+
+class BookingModelAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'booking_start_date', 'booking_end_date', 'is_approved']
+
+
+admin.site.register(Car)
+admin.site.register(Booking, BookingModelAdmin)
--- frontend/models.py
+++ frontend/models.py
@@ -15,6 +15,9 @@ class Car(models.Model):
     def get_absolute_url(self):
         return reverse('car-details', kwargs={'pk': self.pk})
 
+    def __str__(self):
+        return self.name
+
 
 class Booking(models.Model):
     car = models.ForeignKey(Car)
